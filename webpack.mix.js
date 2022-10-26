6b08eaa1cbc9f5e5a2fb0ab587c54d0f1e157163
iaminsleep <xenydok1292@gmail.com>
2022-10-26 18:24:17 +0300

    updated webpack.mix.js, added concurrently for easier development

---
+++ webpack.mix.js
@@ -0,0 +1,29 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+// combine all the styles into a single file
+
+mix.styles([
+    'resources/assets/admin/plugins/fontawesome-free/css/all.min.css',
+    'resources/assets/admin/css/adminlte.min.css',
+], 'public/assets/admin/css/admin.css');
+
+// combine all the scripts into a single file
+
+mix.scripts([
+    'resources/assets/admin/plugins/jquery/jquery.min.js',
+    'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js',
+    'resources/assets/admin/js/adminlte.min.js',
+    'resources/assets/admin/js/demo.js',
+], 'public/assets/admin/js/admin.js');
+
