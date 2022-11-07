c6ba39d5cce1ee8704d57313849ed9304122db8d
iaminsleep <xenydok1292@gmail.com>
2022-11-07 19:58:53 +0300

    edited sidebar data display and caching of this data in appserviceprovider

--- resources/views/front/posts/layouts/sidebar.blade.php
+++ resources/views/front/posts/layouts/sidebar.blade.php
@@ -1,35 +1,19 @@
 <div class="sidebar">
     <div class="widget">
-        <h2 class="widget-title">Recent Posts</h2>
+        <h2 class="widget-title">Popular Posts</h2>
         <div class="blog-list-widget">
             <div class="list-group">
-                <a href="marketing-single.html"
-                    class="list-group-item list-group-item-action flex-column align-items-start">
-                    <div class="w-100 justify-content-between">
-                        <img src="public/assets/front/upload/small_07.jpg" alt="" class="img-fluid float-left">
-                        <h5 class="mb-1">5 Beautiful buildings you need to before dying</h5>
-                        <small>12 Jan, 2016</small>
-                    </div>
-                </a>
-
-                <a href="marketing-single.html"
-                    class="list-group-item list-group-item-action flex-column align-items-start">
-                    <div class="w-100 justify-content-between">
-                        <img src="public/assets/front/upload/small_08.jpg" alt="" class="img-fluid float-left">
-                        <h5 class="mb-1">Let's make an introduction for creative life</h5>
-                        <small>11 Jan, 2016</small>
-                    </div>
-                </a>
-
-                <a href="marketing-single.html"
-                    class="list-group-item list-group-item-action flex-column align-items-start">
-                    <div class="w-100 last-item justify-content-between">
-                        <img src="public/assets/front/upload/small_09.jpg" alt="" class="img-fluid float-left">
-                        <h5 class="mb-1">Did you see the most beautiful sea in the world?
-                        </h5>
-                        <small>07 Jan, 2016</small>
-                    </div>
-                </a>
+                @foreach ($popular_posts as $post)
+                    <a href="{{ route('posts.single', ['slug' => $post->slug]) }}"
+                        class="list-group-item list-group-item-action flex-column align-items-start">
+                        <div class="w-100 justify-content-between">
+                            <img src="{{ $post->getImage() }}" alt="{{ $post->title }}" class="img-fluid float-left">
+                            <h5 class="mb-1">{{ $post->title }}</h5>
+                            <small>{{ $post->getDate() }}</small>
+                            <small>| <i class="fa fa-eye"></i> {{ $post->views }}</small>
+                        </div>
+                    </a>
+                @endforeach
             </div>
         </div><!-- end blog-list -->
     </div><!-- end widget -->
@@ -63,13 +47,12 @@
         <h2 class="widget-title">Popular Categories</h2>
         <div class="link-widget">
             <ul>
-                <li><a href="#">Marketing <span>(21)</span></a></li>
-                <li><a href="#">SEO Service <span>(15)</span></a></li>
-                <li><a href="#">Digital Agency <span>(31)</span></a></li>
-                <li><a href="#">Make Money <span>(22)</span></a></li>
-                <li><a href="#">Blogging <span>(66)</span></a></li>
-                <li><a href="#">Entertaintment <span>(11)</span></a></li>
-                <li><a href="#">Video Tuts <span>(87)</span></a></li>
+                @foreach ($popular_categories as $category)
+                    <li>
+                        <a href="{{ route('categories.single', ['slug' => $category->slug]) }}">{{ $category->title }}
+                            <span>({{ $category->posts_count }})</span></a>
+                    </li>
+                @endforeach
             </ul>
         </div><!-- end link-widget -->
     </div><!-- end widget -->
